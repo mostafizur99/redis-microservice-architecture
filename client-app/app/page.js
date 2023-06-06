b97da7ed96010b0a2d24740caff9d3a4d546f0d5
mostafizur99 <coder.mr11@gmail.com>
2023-06-06 09:44:12 +0600

    feat: initial client project

---
+++ client-app/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center p-24">
+      <h2 className="text-cyan-600">Hello Client</h2>
+    </main>
+  );
+}
